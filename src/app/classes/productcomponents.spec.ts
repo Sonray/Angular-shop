c06f4418a2596386234a76db6306ca801c07e3fb
David Okwacha <osweredavid@gmail.com>
2020-11-06 02:55:36 +0300

    made product class

---
+++ src/app/classes/productcomponents.spec.ts
@@ -0,0 +1,7 @@
+import { Productcomponents } from './productcomponents';
+
+describe('Productcomponents', () => {
+  it('should create an instance', () => {
+    expect(new Productcomponents()).toBeTruthy();
+  });
+});
