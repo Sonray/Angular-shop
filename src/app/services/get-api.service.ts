924e1207451e1eae1403e258bdd52f1bbcdc5cbd
David Okwacha <osweredavid@gmail.com>
2020-11-06 03:29:13 +0300

    made cal to the https

--- src/app/services/get-api.service.ts
+++ src/app/services/get-api.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetAPIService {
 
-  constructor() { }
+  constructor(private httpclient : HttpClient) { }
+
+  getProduct():Observable<any>{
+      return this.httpclient.get("https://hood9000.herokuapp.com/api/products/all/")
+  }
 }
